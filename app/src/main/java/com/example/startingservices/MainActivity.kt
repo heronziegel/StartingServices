ae6a74f5539b526e1a4e825c0298c562d6f73ced
heronziegel <tuo12440@temple.edu>
2023-11-13 15:37:55 -0500

    Added TimerService, created compile error

--- app/src/main/java/com/example/startingservices/MainActivity.kt
+++ app/src/main/java/com/example/startingservices/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.startingservices
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.countdownButton).setOnClickListener{
+            startService(Intent(this,TimerService::class.java))
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/example/startingservices/TimerService.kt
@@ -0,0 +1,34 @@
+package com.example.startingservices
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class TimerService : Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        runTimer()
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    fun runTimer(){
+        Thread {
+            for(i in 20 downTo 0){
+                Log.d("Countdown", i.toString())
+                Thread.sleep(1000)
+        }
+            stopSelf()
+        }.start()
+    }
+
+    override fun onDestroy(){
+        Log.d("Service State", "STOPPED")
+        super.onDestroy()
+    }
+}
